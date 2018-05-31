d73ad7dcf4f98e7dc0e857c180cfd0331ee16476
Ilya Petrov <ilya.petrov.m@gmail.com>
2018-05-31 20:42:16 +0300

    вызов тестовой функции сразу

--- tests/testExpedition.js
+++ tests/testExpedition.js
@@ -1,9 +1,17 @@
 import Expedition from '../js/Expedition.js';
+function TestExpedition(){
 
 function testConstructor(){
   let ws = new WebSocket();
   let ex = new Expedition('title1', wsClient);
   console.assert(ex.getTitle() === 'title1', 'Title test');
   console.assert(ex.getWsClient() === ws, 'ws clients differs');
+})();
+
+function runTests() {
+for(let f in this){
+  f.call();
 }
-testConstructor();
+}
+
+}
